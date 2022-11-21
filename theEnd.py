476aaf094f4a230a447ef4e448274aaf7a822c3a
Ahmet Enis Han KÖSE <116181423+EnisHanKOSE@users.noreply.github.com>
2022-11-21 17:45:55 +0300

    Add files via upload

---
+++ theEnd.py
@@ -0,0 +1,8 @@
+from ince import *
+from itopya import *
+from sinerji import *
+import datetime
+final_ince()
+final_itopya()
+final_sinerji()
+date=datetime.datetime.now().replace(second=0, microsecond=0)
